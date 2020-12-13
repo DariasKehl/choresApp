5dadf1c75c73e8e694d2f48d27ccac98d1ecf376
DariasKehl <neocount@gmail.com>
2020-12-13 11:58:10 -0500

    Working Env

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_12_09_201729) do
+
+  create_table "chores", force: :cascade do |t|
+    t.string "name"
+    t.integer "owner"
+    t.string "description"
+    t.datetime "due_date"
+    t.boolean "completed"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+  end
 
 end
